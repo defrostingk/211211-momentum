3b7b9fb5dda27d9afc8997058e34c151f1045b59
defrostingk <haedong1111@gmail.com>
2021-12-11 15:09:54 +0900

    #2.11 Returns

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const calculator = {
   add: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
   },
   multi: function (a, b) {
-    console.log(a * b);
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
   power: function (a, b) {
-    console.log(a ** b);
+    return a ** b;
   },
 };
 
-calculator.add(2, 3);
-calculator.minus(2, 3);
-calculator.multi(2, 3);
-calculator.divide(2, 3);
-calculator.power(2, 3);
+const plusResult = calculator.add(2, 3);
+console.log(plusResult);
+const minusResult = calculator.minus(plusResult, 10);
+console.log(minusResult);
+const multiResult = calculator.multi(10, minusResult);
+console.log(multiResult);
+const divideResult = calculator.divide(multiResult, plusResult);
+console.log(divideResult);
+const powerResult = calculator.power(divideResult, minusResult);
+console.log(powerResult);
