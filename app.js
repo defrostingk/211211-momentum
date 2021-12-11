b0c2ab56958b8623cf54f1b10f7bc4bff5777821
defrostingk <haedong1111@gmail.com>
2021-12-11 10:53:28 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var a = 10;
-const b = 2;
-let myName = "haedong";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "kimhaedong";
-console.log("your new name is  " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
