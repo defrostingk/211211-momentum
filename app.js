c35adadb34c13cb0975c7013f18d97056318db69
defrostingk <haedong1111@gmail.com>
2021-12-11 11:39:28 +0900

    #2.6 Objects

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const toBuy = ["potato", "tomato", "pizza"];
+const player = {
+  name: "haedong",
+  points: 10,
+  fat: true,
+};
 
-console.log(toBuy[2]);
+console.log(player);
+player.points = player.points + 15;
+console.log(player);
+player.points += 15;
+
+player.lastName = ["potato", "tomato"];
+
+console.log(player);
