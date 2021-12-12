815ac69a08f064109a4ab7acf2782d87862c16db
defrostingk <haedong1111@gmail.com>
2021-12-12 20:10:18 +0900

    #3.8 CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const h1 = document.querySelector(".hello h1");
 
-console.dir(h1);
-
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-  h1.style.color = newColor;
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
