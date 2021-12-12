45035b7e86cf66fd5ecd649d6c1704a0aa0cba64
defrostingk <haedong1111@gmail.com>
2021-12-12 18:59:45 +0900

    #3.5 More Events

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
-const title = document.querySelector(".hello h1");
-// const title = document.getElementById("hello");
-title.innerText = "Hello";
-console.log(title);
+const h1 = document.querySelector(".hello h1");
+
+console.dir(h1);
+
+function handleTitleClick() {
+  if (h1.style.color == "blue") {
+    h1.style.color = "black";
+  } else {
+    h1.style.color = "blue";
+  }
+}
+
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD");
+}
+
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
