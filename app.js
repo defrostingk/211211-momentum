3ff295ade43118972ae6246d714b9e08027bf773
defrostingk <haedong1111@gmail.com>
2021-12-12 19:38:23 +0900

    #3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -3,42 +3,14 @@ const h1 = document.querySelector(".hello h1");
 console.dir(h1);
 
 function handleTitleClick() {
-  if (h1.style.color == "blue") {
-    h1.style.color = "black";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
   } else {
-    h1.style.color = "blue";
+    newColor = "blue";
   }
+  h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
